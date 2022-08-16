96481299cb42af545c58a673c2451a62d11dce43
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-08-16 20:38:06 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ function distanceFromHqInBlocks(distance) {
     const traveled = distanceTravelledInFeet(start, destination)
     if (traveled < 400) {
       return 0
-    } else if (traveled > 401 && traveled < 2000) {
-      return traveled * 0.02
-    } else if (traveled < 2500) {
+    } else if ((traveled > 400) && (traveled < 2000)) {
+      return (traveled-400) * 0.02 
+    } else if ((traveled > 2000) && (traveled < 2500)) {
       return 25
     } else if (traveled > 2500) {
       return 'cannot travel that far'
