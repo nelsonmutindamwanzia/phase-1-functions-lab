e55037431ee3516764ca377e943eb18d30f01934
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-08-16 15:48:58 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+    return Math.abs(distance - 42)
+
+  }
+
+  function distanceFromHqInFeet(distance) {
+    return distanceFromHqInBlocks(distance) * 264
+  }
+
+  function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264
+  }
+
+  function calculatesFarePrice(start, destination) {
+    const traveled = distanceTravelledInFeet(start, destination)
+    if (traveled < 400) {
+      return 0
+    } else if (traveled > 401 && traveled < 2000) {
+      return traveled * 0.02
+    } else if (traveled < 2500) {
+      return 25
+    } else if (traveled > 2500) {
+      return 'cannot travel that far'
+    }
+  }
